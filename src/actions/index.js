8d35ce733b7a9ccf2fdce873e1fd7847eacaf6c1
lukostry <lukasz.ostrowski85@gmail.com>
2017-06-04 18:46:48 +0200

    Replace isomorphic-fetch with axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,12 @@
-import fetch from 'isomorphic-fetch';
+import axios from 'axios';
 import { v4 } from 'uuid';
 
 const BASE_URL = 'http://api.wordnik.com:80/v4/words.json/randomWord?hasDictionaryDef=false&minCorpusCount=0&maxCorpusCount=-1&minDictionaryCount=1&maxDictionaryCount=-1&minLength=5&maxLength=10&api_key=a2a73e7b926c924fad7001ca3111acd55af2ffabf50eb4ae5';
 
 export const fetchNewWord = () => dispatch =>
-    fetch(BASE_URL)
-        .then(response => response.json())
-        .then((json) => {
-            const wordConvertedToArr = json.word.toUpperCase().split('');
+    axios.get(BASE_URL)
+        .then((response) => {
+            const wordConvertedToArr = response.data.word.toUpperCase().split('');
 
             dispatch({
                 type: 'GAME_RELOADED',
