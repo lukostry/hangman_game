214b071e91dc3e8b49243a02921be94b319f7f6b
lukostry <lukasz.ostrowski85@gmail.com>
2017-06-04 21:58:59 +0200

    Workaround for two words sentences returned by Word API

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,24 @@
 import axios from 'axios';
 import { v4 } from 'uuid';
 
+const MAX_LENGTH_OF_WORD = 10;
 const API_CONFIG = {
     headers: { 'X-Mashape-Key': 'u5gGqvyur4mshRDxulUaySLKfIYhp1jF4objsnJefU3WAntnmX' },
 };
-
 const WORD_URL = 'https://wordsapiv1.p.mashape.com/words/?lettersMax=10&random=true';
 
 export const fetchNewWord = () => dispatch =>
     axios.get(WORD_URL, API_CONFIG)
         .then((response) => {
-            const wordConvertedToArr = response.data.word.toUpperCase().split('');
+            let wordFetched = response.data.word;
+
+            if (wordFetched.length > MAX_LENGTH_OF_WORD) {
+                // In very rare cases Word API could return two words sentence which length exceeds
+                // 10 characters. As a temporary solution trim this sentence.
+                wordFetched = wordFetched.substr(0, MAX_LENGTH_OF_WORD);
+            }
+
+            const wordConvertedToArr = wordFetched.toUpperCase().split('');
 
             dispatch({
                 type: 'GAME_RELOADED',
